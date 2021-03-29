91eb30e829062bfe1f35c4c94076b3d448c1afd7
Dian <anindita.dian09@gmail.com>
2021-03-29 22:36:15 +0700

    add argument events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Parentbox">
+      <FotoProduk />
+      <ProdukInfo category="SKATEBOARDING" name="ComfyCush Old Skool" />
     </div>
   );
 }
 
+function FotoProduk() {
+  return (
+      <div className="Foto">
+        <img alt="sneaker" src="vans.png" />
+      </div>
+  );
+}
+
+function ProdukInfo(props) {
+  const { category, name } = props;
+
+  return (
+    <div>
+      <div className="Deskripsi">
+        <p className="Oldskool">{category}</p>
+        <h1 className="Title">{name}</h1>
+        <p className="Price">IDR 999.999</p>
+        <p className="Info">Vans has reinvigorated the classNameic Old Skool by introducing ComfyCush: a new softer, cushiony outsole that gives the ComfyCush Old Skool a first-className fit that feels like walking on a cloud. Newly constructed canvas and suede uppers house simplified one-piece interiors with added arch support and new moisture-wicking lining throughout, resulting in a lightweight experience that assures you’ll Keep It Comfy at all times.</p>
+        <a onClick={(e) => TambahCart(name, e)} href="#">Add to cart </a>
+      </div>
+    </div>
+  );
+
+}
+
+function TambahCart(e) {
+  console.log("Membeli " + e);
+}
 export default App;
